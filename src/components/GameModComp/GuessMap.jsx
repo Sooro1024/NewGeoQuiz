d284a6cd605023210e190c02d6c344c8bf60ab5d
Sooro1024 <soorogorgoyan@gmail.com>
2019-03-24 17:33:25 +0400

    done

--- src/components/GameModComp/GuessMap.jsx
+++ src/components/GameModComp/GuessMap.jsx
@@ -1,100 +1,117 @@
-import React from 'react';
+import React from "react";
 import {
-    Button,
-    Grow,
-    LinearProgress,
-    Typography,
-    Slide
+  Button,
+  Grow,
+  LinearProgress,
+  Typography,
+  Slide
 } from "@material-ui/core";
 import Timer from "./Timer";
 import ResultOfGame from "./ResultOfGame";
-import MapComp from './MapComp';
+import MapComp from "./MapComp";
 
-  const AnswerBox = (dataForGame, gameArray, progress) => {
-    let arrOfAnswer = [];
-    arrOfAnswer.push(gameArray[progress]);
-    for (let i = 1; arrOfAnswer.length < 4; i++) {
-      let el = Math.floor(Math.random() * dataForGame.length);
-      if (!arrOfAnswer.some(e => e.name === dataForGame[el].name)) {
-        arrOfAnswer.push(dataForGame[el]);
-      }
+const AnswerBox = (dataForGame, gameArray, progress) => {
+  let arrOfAnswer = [];
+  arrOfAnswer.push(gameArray[progress]);
+  for (let i = 1; arrOfAnswer.length < 4; i++) {
+    let el = Math.floor(Math.random() * dataForGame.length);
+    if (!arrOfAnswer.some(e => e.name === dataForGame[el].name)) {
+      arrOfAnswer.push(dataForGame[el]);
     }
-    return arrOfAnswer;
-  };
-
-
+  }
+  return arrOfAnswer;
+};
 
 const GuessMap = ({
-    gameArray,
-    dataForGame,
-    progress,
-    CheckTheAnswer,
-    result,
-    onTime,
-    gameMod
+  gameArray,
+  dataForGame,
+  progress,
+  CheckTheAnswer,
+  result,
+  onTime,
+  gameMod
 }) => {
-    const percent = Math.round(progress * 7.143);
-    if (gameArray.length === 0) {
-        return <div />;
-    } else {
-        return (
-            <>
-                <Slide
-                    direction="down"
-                    in={progress === 15}
-                    timeout={{ enter: 800, exit: 200 }}
-                    unmountOnExit
-                    mountOnEnter
-                >
-                    <ResultOfGame result={result} gameMod={gameMod} />
-                </Slide>
-                <Grow in={gameArray[progress] !== undefined} mountOnEnter unmountOnExit>
-                <>
-                <div style={{display: 'flex'}}>
-                    {gameArray[progress] !== undefined && <MapComp gameArray={gameArray} progress={progress} />}
-                    <div style={{display: 'flex', flexDirection: 'column'}}>
-                    {onTime && gameArray[progress] !== undefined && (
-                <Timer
-                  result={result}
-                  progress={progress}
-                  CheckTheAnswer={CheckTheAnswer}
-                />
+  const percent = Math.round(progress * 7.143);
+  if (gameArray.length === 0) {
+    return <div />;
+  } else {
+    return (
+      <>
+        <Slide
+          direction="down"
+          in={progress === 15}
+          timeout={{ enter: 800, exit: 200 }}
+          unmountOnExit
+          mountOnEnter
+        >
+          <ResultOfGame result={result} gameMod={gameMod} />
+        </Slide>
+        <Grow in={gameArray[progress] !== undefined} mountOnEnter unmountOnExit>
+          <>
+            {gameArray[progress] !== undefined && (
+              <LinearProgress
+                variant="determinate"
+                color="secondary"
+                value={percent}
+                style={{ marginBottom: "5px" }}
+              />
+            )}
+            <div style={{ display: "flex" }}>
+              {gameArray[progress] !== undefined && (
+                <MapComp gameArray={gameArray} progress={progress} />
               )}
-              {!onTime && (
-                <Typography
-                  variant="h3"
-                  color="primary"
-                  style={{ userSelect: "none" }}
-                >
-                  {result}
-                </Typography>
-              )}
-              <div style={{display: 'flex', flexDirection: 'column'}}>
-                {gameArray[progress] !== undefined &&
-                  AnswerBox(dataForGame, gameArray, progress)
-                    .sort((a, b) => {
-                      return 0.5 - Math.random();
-                    })
-                    .map(el => (
-                      <Button
-                        style={{ marginBottom: "10px" }}
-                        onClick={() => CheckTheAnswer(`${el.name}`)}
-                        variant="contained"
-                        color="primary"
-                        key={el.name}
-                      >
-                        {el.name}
-                      </Button>
-                    ))}
-                    </div>
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                  maxWidth: "350px",
+                  minWidth: "350px",
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                  margin: "30px"
+                }}
+              >
+                {onTime && gameArray[progress] !== undefined && (
+                  <Timer
+                    result={result}
+                    progress={progress}
+                    CheckTheAnswer={CheckTheAnswer}
+                  />
+                )}
+                {!onTime && gameArray[progress] !== undefined && (
+                  <Typography
+                    variant="h3"
+                    color="primary"
+                    style={{ userSelect: "none", marginBottom: '80px' }}
+                  >
+                    {result}
+                  </Typography>
+                )}
+                <div style={{ display: "flex", flexDirection: "column", justifyContent: 'center', marginTop: '30px', minWidth: '250px' }}>
+                  {gameArray[progress] !== undefined &&
+                    AnswerBox(dataForGame, gameArray, progress)
+                      .sort((a, b) => {
+                        return 0.5 - Math.random();
+                      })
+                      .map(el => (
+                        <Button
+                          style={{ marginBottom: "10px" }}
+                          onClick={() => CheckTheAnswer(`${el.name}`)}
+                          variant="contained"
+                          color="primary"
+                          key={el.name}
+                        >
+                          {el.name}
+                        </Button>
+                      ))}
+                </div>
               </div>
-              </div>
-              {gameArray[progress] !== undefined && <LinearProgress variant="determinate" color='secondary' value={percent} />}
-              </>
-                </Grow>
-            </>
-            )
-    }
-}
+            </div>
+          </>
+        </Grow>
+      </>
+    );
+  }
+};
 
-export default GuessMap
+export default GuessMap;
--- src/components/GameModComp/MapComp.jsx
+++ src/components/GameModComp/MapComp.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import {
   ComposableMap,
   ZoomableGroup,
@@ -50,84 +50,59 @@ const defContry = {
   }
 };
 
-export default class MapComp extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-        // FocusCountry: 0,
-    };
-  }
-
-
-
-//   shouldComponentUpdate(nextProps){
-//     return this.props.progress !== nextProps.progress ? true : false 
-//   }
-
-//   componentDidMount(){
-//       this.setState({FocusCountry: this.props.gameArray[this.props.progress].alpha2Code})
-//   }
-
-  // componentDidUpdate(prevProps){
-  //     if(this.props.progress !== prevProps.progress){
-  //       this.forceUpdate()
-  //   }
-  // }
-
-
-  render() {
-    const { gameArray, progress } = this.props;
-    return (
-      <Card
-        style={{ maxHeight: "650px", maxWidth: "900px", marginLeft: "20px" }}
-        raised
-      >
-        <CardHeader
-          style={{ padding: "16px 16px 0px 16px" }}
-          title="Which country is it ?"
-          subheader={`Question ${progress + 1}`}
-        />
-        <CardContent style={{ paddingTop: "0" }}>
-          <ComposableMap
-            projectionConfig={{
-              scale: 180,
-              xOffset: -20,
-              yOffset: -42,
-              rotation: [-11, 0, 0]
-            }}
-            width={800}
-            height={600}
-            style={{
-              width: "100%",
-              height: "auto",
-              preserveAspectRatio: "none"
-            }}
-          >
-            <ZoomableGroup center={[0, 20]} disablePanning>
-              <Geographies geography={WorldMap} disableOptimization>
-                {(geographies, projection) =>
-                  geographies.map(
-                    (geography, i) =>
-                      geography.id !== "ATA" && (
-                        <Geography
-                          key={i}
-                          geography={geography}
-                          projection={projection}
-                          style={
-                            gameArray[progress].alpha2Code ===
-                            geography.properties.ISO_A2
-                              ? focusContry
-                              : defContry
-                          }
-                        />
-                      )
-                  )
-                }
-              </Geographies>
-            </ZoomableGroup>
-          </ComposableMap>
-        </CardContent>
-      </Card>
-    );
-  }
+const MapComp = ({ gameArray, progress }) => {
+  return (
+    <Card
+      style={{ maxHeight: "650px", maxWidth: "900px", marginLeft: "20px" }}
+      raised
+    >
+      <CardHeader
+        style={{ padding: "16px 16px 0px 16px" }}
+        title="Which country is it ?"
+        subheader={`Question ${progress + 1}`}
+      />
+      <CardContent style={{ paddingTop: "0" }}>
+        <ComposableMap
+          projectionConfig={{
+            scale: 180,
+            xOffset: -20,
+            yOffset: -20,
+            rotation: [-11, 0, 0]
+          }}
+          width={800}
+          height={600}
+          style={{
+            width: "100%",
+            height: "auto",
+            preserveAspectRatio: "none"
+          }}
+        >
+          <ZoomableGroup center={[0, 20]} disablePanning>
+            <Geographies geography={WorldMap} disableOptimization>
+              {(geographies, projection) =>
+                geographies.map(
+                  (geography, i) =>
+                    geography.id !== "ATA" && (
+                      <Geography
+                        key={i}
+                        geography={geography}
+                        projection={projection}
+                        style={
+                          gameArray[progress].alpha2Code ===
+                          geography.properties.ISO_A2
+                            ? focusContry
+                            : defContry
+                        }
+                      />
+                    )
+                )
+              }
+            </Geographies>
+          </ZoomableGroup>
+        </ComposableMap>
+      </CardContent>
+    </Card>
+  )
 }
+
+export default MapComp;
