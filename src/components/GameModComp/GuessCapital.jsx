dd7df696be93993f8c8566e282f84c5b202696a2
Sooro1024 <soorogorgoyan@gmail.com>
2019-04-01 18:32:11 +0400

    Here I try to correct some extra reRenders in Mainwindow component with react hooks.

--- src/components/GameModComp/GuessCapital.jsx
+++ src/components/GameModComp/GuessCapital.jsx
@@ -1,110 +1,121 @@
-import React from "react";
-import {
-  Card,
-  CardContent,
-  CardHeader,
-  Button,
-  Grow,
-  LinearProgress,
-  Typography,
-  Slide
-} from "@material-ui/core";
-import GuessFlagStyles from "./GuessFlagStyles";
-import Timer from "./Timer";
-import ResultOfGame from "./ResultOfGame";
+import { Button, Card, CardContent, CardHeader, Grow, LinearProgress, Slide, Typography } from "@material-ui/core";
+import React, { useEffect, useState } from "react";
+import { AnswerBox, IncrementResult } from "./functionModuls";
 import GuessCapitalPhoto from "./GuessCapitalPhoto";
-import { AnswerBox } from "./AnswerBox";
+import GuessFlagStyles from "./GuessFlagStyles";
+import ResultOfGame from "./ResultOfGame";
+import Timer from "./Timer";
 
 const styles = GuessFlagStyles;
 
 const GuessCapital = ({
-  gameArray,
-  dataForGame,
-  progress,
-  CheckTheAnswer,
-  result,
-  onTime,
-  gameMod,
-  wrong
+	gameArray,
+	dataForGame,
+	onTime,
+	gameMod,
+	gameDiff
 }) => {
-  const percent = Math.round(progress * 7.143);
-  if (gameArray.length === 0) {
-    return <div />;
-  } else if (gameArray[progress] !== undefined) {
-    return (
-      <>
-        <Grow in={gameArray[progress] !== undefined} mountOnEnter unmountOnExit>
-          <div>
-            <div style={styles.cardAndProgressWrapper}>
-              <Card style={{ minWidth: "550px" }}>
-                <CardHeader
-                  title={`Which country capital is ${
-                    gameArray[progress].capital
-                  } ?`}
-                  subheader={`Question ${progress + 1}`}
-                />
-                <CardContent style={{ paddingTop: "0" }}>
-                    <GuessCapitalPhoto
-                      gameData={gameArray[progress]}
-                      styles={styles.CardMedia}
-                      progress={progress}
-                    />
-                </CardContent>
-              </Card>
-              {onTime && (
-                <Timer
-                  result={result}
-                  progress={progress}
-                  CheckTheAnswer={CheckTheAnswer}
-                />
-              )}
-              {!onTime && (
-                <Typography
-                  variant="h3"
-                  color="primary"
-                  style={{ userSelect: "none" }}
-                >
-                  {result}
-                </Typography>
-              )}
-            </div>
-            <LinearProgress variant="determinate" value={percent} />
-            <div style={styles.AnswerBoxWrapper}>
-              <div style={styles.AnswerBox}>
-                {AnswerBox(dataForGame, gameArray, progress)
-                  .sort((a, b) => {
-                    return 0.5 - Math.random();
-                  })
-                  .map(el => (
-                    <Button
-                      style={{ marginBottom: "10px" }}
-                      onClick={() => CheckTheAnswer(`${el.name}`)}
-                      variant="contained"
-                      color="primary"
-                      key={el.name}
-                    >
-                      {el.name}
-                    </Button>
-                  ))}
-              </div>
-            </div>
-          </div>
-        </Grow>
-      </>
-    );
-  } else {
-    return (
-      <Slide
-        direction="down"
-        in={progress === 15}
-        timeout={{ enter: 800, exit: 200 }}
-        unmountOnExit
-        mountOnEnter
-      >
-        <ResultOfGame result={result} gameMod={gameMod} wrong={wrong} />
-      </Slide>
-    );
-  }
+	const [state, setState] = useState([]);
+	useEffect(() => {
+		setState([]);
+	}, [gameArray]);
+
+	function CheckTheAnswer(ev) {
+		if (ev === gameArray[state.length].name) {
+			setState([...state, true]);
+		} else {
+			setState([...state, false]);
+		}
+	}
+
+	const result = IncrementResult(onTime, state, gameDiff);
+	const wrong = () =>
+		state
+			.map((el, index) => (!el ? gameArray[index].name : null))
+			.filter(value => value !== null);
+
+	const percent = Math.round(state.length * 7.143);
+	if (gameArray.length === 0) {
+		return <div />;
+	} else if (gameArray[state.length] !== undefined) {
+		return (
+			<>
+				<Grow
+					in={gameArray[state.length] !== undefined}
+					mountOnEnter
+					unmountOnExit
+				>
+					<div>
+						<div style={styles.cardAndProgressWrapper}>
+							<Card style={{ minWidth: "550px" }}>
+								<CardHeader
+									title={`Which country capital is ${
+										gameArray[state.length].capital
+									} ?`}
+									subheader={`Question ${state.length + 1}`}
+								/>
+								<CardContent style={{ paddingTop: "0" }}>
+									<GuessCapitalPhoto
+										gameData={gameArray[state.length]}
+										styles={styles.CardMedia}
+										progress={state.length}
+									/>
+								</CardContent>
+							</Card>
+							{onTime && (
+								<Timer
+									result={result}
+									progress={state.length}
+									CheckTheAnswer={CheckTheAnswer}
+								/>
+							)}
+							{!onTime && (
+								<Typography
+									variant="h3"
+									color="primary"
+									style={{ userSelect: "none" }}
+								>
+									{result}
+								</Typography>
+							)}
+						</div>
+						<LinearProgress variant="determinate" value={percent} />
+						<div style={styles.AnswerBoxWrapper}>
+							<div style={styles.AnswerBox}>
+								{AnswerBox(dataForGame, gameArray, state.length)
+									.sort((a, b) => {
+										return 0.5 - Math.random();
+									})
+									.map(el => (
+										<Button
+											style={{ marginBottom: "10px" }}
+											onClick={() => CheckTheAnswer(`${el.name}`)}
+											variant="contained"
+											color="primary"
+											key={el.name}
+										>
+											{el.name}
+										</Button>
+									))}
+							</div>
+						</div>
+					</div>
+				</Grow>
+			</>
+		);
+	} else {
+		return (
+			<Slide
+				direction="down"
+				in={state.length === 15}
+				timeout={{ enter: 800, exit: 200 }}
+				unmountOnExit
+				mountOnEnter
+			>
+				<ResultOfGame result={result} gameMod={gameMod} wrong={wrong()} />
+			</Slide>
+		);
+	}
 };
 
-export default GuessCapital
+export default GuessCapital;
--- src/components/Mainwindow.jsx
+++ src/components/Mainwindow.jsx
@@ -1,180 +1,138 @@
-import React, { Component } from "react";
-import ChooseAndPlay from "./ChooseAndPlay";
-import MyAppBar from "./MyAppBar";
-import GuessFlag from "./GameModComp/GuessFlag";
-import ModIsUndefined from "./GameModComp/ModIsUndefined";
 import Slide from "@material-ui/core/Slide";
-import GuessCapital from "./GameModComp/GuessCapital";
+import React, { Component } from "react";
 import AboutUs from "./AboutUs";
+import ChooseAndPlay from "./ChooseAndPlay";
+import GuessCapital from "./GameModComp/GuessCapital";
+import GuessFlag from "./GameModComp/GuessFlag";
 import GuessMap from "./GameModComp/GuessMap";
+import ModIsUndefined from "./GameModComp/ModIsUndefined";
+import MyAppBar from "./MyAppBar";
 
 export default class Mainwindow extends Component {
-  constructor(props) {
-    super(props);
+	constructor(props) {
+		super(props);
 
-    this.state = {
-      value: 0,
-      openDialog: false,
-      gameMod: "0",
-      gameDiff: "0",
-      onTime: false,
-      start: false,
-      gameArray: [],
-      dataForGame: [],
-      progress: 0,
-      result: 0,
-      wrong: []
-    };
-  }
+		this.state = {
+			value: 0,
+			openDialog: false,
+			gameMod: "0",
+			gameDiff: "0",
+			onTime: false,
+			start: false,
+			gameArray: [],
+			dataForGame: []
+		};
+	}
 
-  IncrementResult = () => {
-    if (this.state.onTime) {
-      if (this.state.gameDiff === '0') {
-        this.setState({ result: this.state.result + 150 })
-      } else if (this.state.gameDiff === '1') {
-        this.setState({ result: this.state.result + 300 })
-      } else {
-        this.setState({ result: this.state.result + 450 })
-      }
-    } else { 
-      if (this.state.gameDiff === '0') {
-        this.setState({ result: this.state.result + 100 })
-      } else if (this.state.gameDiff === '1') {
-        this.setState({ result: this.state.result + 200 })
-      } else {
-        this.setState({ result: this.state.result + 300 })
-      }
-    }
-  }
+	ChooseGameMod = event => {
+		this.setState({ gameMod: event.target.value });
+	};
 
-  CheckTheAnswer = event => {
-    this.setState({ progress: this.state.progress + 1 });
-    if (this.state.gameArray[this.state.progress].name === event) {
-      this.IncrementResult();
-    } else {
-      this.setState({wrong: [...this.state.wrong, this.state.gameArray[this.state.progress].name]})
-    }
-  };
+	ChooseGameDiff = event => {
+		this.setState({ gameDiff: event.target.value });
+	};
 
-  ChooseGameMod = event => {
-    this.setState({ gameMod: event.target.value });
-  };
+	HandleChanges = name => ev => {
+		this.setState({ [name]: !this.state[name] });
+		if (name === "start") {
+			this.setState({
+				openDialog: !this.state.openDialog,
+				value: this.state.gameMod - 0 + 1
+			});
+		}
+	};
 
-  ChooseGameDiff = event => {
-    this.setState({ gameDiff: event.target.value });
-  };
+	HandleAppBarValeuChange = (event, value) => {
+		this.setState({
+			value,
+			gameArray: [],
+			dataForGame: [],
+			onTime: false
+		});
+		if (4 > value && value > 0) {
+			this.setState({
+				openDialog: !this.state.openDialog,
+				gameMod: `${value - 1}`
+			});
+		}
+	};
 
-  HandleChanges = name => ev => {
-    this.setState({ [name]: !this.state[name] });
-    if (name === "start") {
-      this.setState({
-        openDialog: !this.state.openDialog,
-        value: this.state.gameMod - 0 + 1
-      });
-    }
-  };
+	ChooseGameType = (gameArray, dataForGame) => {
+		this.setState({ gameArray, dataForGame });
+	};
 
-  HandleAppBarValeuChange = (event, value) => {
-    this.setState({
-      value,
-      gameArray: [],
-      dataForGame: [],
-      progress: 0,
-      result: 0,
-      onTime: false,
-      wrong: []
-    });
-    if (4 > value && value > 0) {
-      this.setState({
-        openDialog: !this.state.openDialog,
-        gameMod: `${value - 1}`
-      });
-    }
-  };
-
-  ChooseGameType = (gameArray, dataForGame) => {
-    this.setState({ gameArray, dataForGame });
-  };
-
-  render() {
-    const {
-      value,
-      openDialog,
-      gameMod,
-      gameDiff,
-      onTime,
-      gameArray,
-      dataForGame,
-      progress,
-      result,
-      wrong
-    } = this.state;
-    return (
-      <>
-        <MyAppBar
-          value={value}
-          HandleAppBarValeuChange={this.HandleAppBarValeuChange}
-        />
-        <div
-          style={{
-            minHeight: "665px",
-            paddingTop: "30px",
-            backgroundColor: "#FAFAFA"
-          }}
-        >
-          <Slide direction="up" in={value === 0} mountOnEnter unmountOnExit>
-            <ModIsUndefined HandleChanges={this.HandleChanges} HandleAppBarValeuChange={this.HandleAppBarValeuChange}/>
-          </Slide>
-          <Slide direction="up" in={value === 1} mountOnEnter unmountOnExit>
-            <GuessMap onTime={onTime}
-            gameArray={gameArray}
-            dataForGame={dataForGame}
-            progress={progress}
-            CheckTheAnswer={this.CheckTheAnswer}
-            result={result}
-            gameMod={gameMod}
-            wrong={wrong} />
-          </Slide>
-          <Slide direction="up" in={value === 2} mountOnEnter unmountOnExit>
-            <GuessFlag
-              onTime={onTime}
-              gameArray={gameArray}
-              dataForGame={dataForGame}
-              progress={progress}
-              CheckTheAnswer={this.CheckTheAnswer}
-              result={result}
-              gameMod={gameMod}
-              wrong={wrong}
-            />
-          </Slide>
-          <Slide direction="up" in={value === 3} mountOnEnter unmountOnExit>
-            <GuessCapital
-            onTime={onTime}
-            gameArray={gameArray}
-            dataForGame={dataForGame}
-            progress={progress}
-            CheckTheAnswer={this.CheckTheAnswer}
-            result={result}
-            gameMod={gameMod}
-            wrong={wrong}
-            />
-          </Slide>
-          <Slide direction="up" in={value === 4} mountOnEnter unmountOnExit>
-            <AboutUs />
-          </Slide>
-        </div>
-        <ChooseAndPlay
-          openDialog={openDialog}
-          gameMod={gameMod}
-          onTime={onTime}
-          gameDiff={gameDiff}
-          ChooseGameMod={this.ChooseGameMod}
-          ChooseGameDiff={this.ChooseGameDiff}
-          HandleChanges={this.HandleChanges}
-          ChooseGameType={this.ChooseGameType}
-          AppBarvalue={value}
-        />
-      </>
-    );
-  }
+	render() {
+		const {
+			value,
+			openDialog,
+			gameMod,
+			gameDiff,
+			onTime,
+			gameArray,
+			dataForGame
+		} = this.state;
+		return (
+			<>
+				<MyAppBar
+					value={value}
+					HandleAppBarValeuChange={this.HandleAppBarValeuChange}
+				/>
+				<div
+					style={{
+						minHeight: "665px",
+						paddingTop: "30px",
+						backgroundColor: "#FAFAFA"
+					}}
+				>
+					<Slide direction="up" in={value === 0} mountOnEnter unmountOnExit>
+						<ModIsUndefined
+							HandleChanges={this.HandleChanges}
+							HandleAppBarValeuChange={this.HandleAppBarValeuChange}
+						/>
+					</Slide>
+					<Slide direction="up" in={value === 1} mountOnEnter unmountOnExit>
+						<GuessMap
+							onTime={onTime}
+							gameArray={gameArray}
+							dataForGame={dataForGame}
+							gameDiff={gameDiff}
+							gameMod={gameMod}
+						/>
+					</Slide>
+					<Slide direction="up" in={value === 2} mountOnEnter unmountOnExit>
+						<GuessFlag
+							onTime={onTime}
+							gameArray={gameArray}
+							dataForGame={dataForGame}
+							gameDiff={gameDiff}
+							gameMod={gameMod}
+						/>
+					</Slide>
+					<Slide direction="up" in={value === 3} mountOnEnter unmountOnExit>
+						<GuessCapital
+							onTime={onTime}
+							gameArray={gameArray}
+							dataForGame={dataForGame}
+							gameDiff={gameDiff}
+							gameMod={gameMod}
+						/>
+					</Slide>
+					<Slide direction="up" in={value === 4} mountOnEnter unmountOnExit>
+						<AboutUs />
+					</Slide>
+				</div>
+				<ChooseAndPlay
+					openDialog={openDialog}
+					gameMod={gameMod}
+					onTime={onTime}
+					gameDiff={gameDiff}
+					ChooseGameMod={this.ChooseGameMod}
+					ChooseGameDiff={this.ChooseGameDiff}
+					HandleChanges={this.HandleChanges}
+					ChooseGameType={this.ChooseGameType}
+					AppBarvalue={value}
+				/>
+			</>
+		);
+	}
 }
